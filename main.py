19244d19cedf8ac0c60c6f11bd220bdf2b563e94
dexterdy <j.p.hagedoorn@proton.me>
2024-02-07 15:24:33 +0100

    lijst van issues

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from PyQt6.QtGui import QGuiApplication
 from PyQt6.QtQml import QQmlApplicationEngine
 from PyQt6.QtCore import QTimer, QObject, pyqtSignal, pyqtSlot
-
+from github import Auth, GithubIntegration
 
 app = QGuiApplication(sys.argv)
 
@@ -11,5 +11,18 @@ engine = QQmlApplicationEngine()
 engine.quit.connect(app.quit)
 engine.load("main.qml")
 
+with open("./private_key.pem", "r") as pem_file:
+    auth = Auth.AppAuth(820437, pem_file.read())
+
+# check wether app we are authenticated
+# if not, open the installation page and authenticate, then save it in a file
+# if yes, get authentication details from saved file
+
+gi = GithubIntegration(auth=auth)
+ins = gi.get_installation("dexterdy", "lightning-timesheet")
+g = ins.get_github_for_installation()
+
+issues = g.get_repo("dexterdy/lightning-timesheet").get_issues()
+print([s.title for s in issues])
 
 sys.exit(app.exec())
