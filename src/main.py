a78150f8f3e4b924ab0bd26e245f3c983eb8d043
dexterdy <j.p.hagedoorn@proton.me>
2024-04-28 14:13:28 +0200

    add imports back in because they are pyside models

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ createBackend()
 import sys
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
+from githubIssuesModel import GithubIssuesModel
+from weekDaysModel import WeekDaysModel, LogsModel
 
 app = QGuiApplication(sys.argv)
 engine = QQmlApplicationEngine()
